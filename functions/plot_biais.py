0112bfeee0f600a6c66a79be5c36dff3592c6c53
Florian Regnault <florian.regnault@scalian.com>
2024-08-29 15:41:26 +0200

    Update plot_biais to the new attributes names + moving to a more PEP-8 code

--- functions/plot_biais.py
+++ functions/plot_biais.py
@@ -5,7 +5,7 @@ def plot_bias(pr):
 
     # Load the data
     error = pr.load_errors()
-    nmodes = pr.nmodes()
+    nmodes = pr.get_nmodes()
     error_list = error.keys()
 
     # Get the time for the xaxis
@@ -13,9 +13,13 @@ def plot_bias(pr):
 
     fig = plt.figure("Bias", clear=True)
     for err in error_list:
-        plt.plot(t, error[err], label=err)
+        plt.plot(
+            t,
+            error[err],
+            label=err
+            )
 
-    plt.ylim(0,1)
+    # plt.ylim(0,1)
     plt.legend(frameon=False)
     plt.xlabel("Time (s)")
     plt.ylabel("Normalised Velocity Error")
@@ -49,7 +53,6 @@ if __name__ == "__main__":
     res_folder="/home/fregnault/data/red_lum_cpp_data/DNS100/ROMDNS-v3.4.1/ITHACAoutput/Reduced_coeff_4_0.001_100_neglectedPressure_centered",
     save_dir="/home/fregnault/data/red_lum_cpp_data/DNS100/ROMDNS-v3.4.1/", verbose=2)
 
+    plot_bias(pr)
 
-    pr.load_meanModes()
-    pr.load_meanModes()
 
