432226c89cf9e67c1d095e9524c6d355249d0147
Florian Regnault <florian.regnault@scalian.com>
2024-09-02 14:44:48 +0200

    Now scaling yaxis according to lambda values (sqrt(eigen) * 1.4) + making plot more pretty

--- functions/plot_modes.py
+++ functions/plot_modes.py
@@ -1,8 +1,12 @@
-from plot_bt_dB_MCMC_varying_error_NoEV import height
+import numpy as np
 from pyredlum import pyRedLUM
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+# import mplcyberpunk
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["#000000", "#E69F00", "#56B4E9","#009E73","#F0E442","#0072B2","#D55E00","CC79A7"])
+# plt.style.use("cyberpunk")
 from icecream import ic
-import tuftelike as tf
 
 def plot_all_modes(pr):
 
@@ -30,7 +34,6 @@ def plot_all_modes(pr):
     ICs = pr.load_ICs()
     eigen = pr.load_lambda()
 
-    print(eigen)
 
     dt = pr.get_dt()
     t = pr.get_t_sim()
@@ -52,7 +55,6 @@ def plot_all_modes(pr):
     nrow += 1
 
     height_ratios.insert(0,0.1)
-    ic(nrow,ncol)
 
     gs = plt.GridSpec(
         nrow,
@@ -64,7 +66,6 @@ def plot_all_modes(pr):
     axs = []
     for i in range(1,nrow):
         for j in range(ncol):
-            ic(i,j)
             axs.append(fig.add_subplot(gs[i, j]))
 
     # Ax containing the legend taking the full first line
@@ -75,7 +76,7 @@ def plot_all_modes(pr):
         axs[n].plot(
             t,
             modesRef[:,n],
-            color="black",
+            color="red",
             linestyle="--",
             label="Reference"
         )
@@ -83,6 +84,7 @@ def plot_all_modes(pr):
         axs[n].plot(
             t,
             meanModes[:, n],
+            color="green",
             label="Mean mode"
         )
 
@@ -92,47 +94,74 @@ def plot_all_modes(pr):
             ICs[n][:,1],
             alpha=0.5,
             color="orange",
+            linewidth=0,
             label="Confidence Interval"
         )
 
     # Cleaning extra axes
     for n in range(nmodes,(nrow-1)*ncol):
-        fig.delaxes(axs[n])
+        axs[n].axis("off")
 
     # Aesthetic things
 
     handles,labels = axs[1].get_legend_handles_labels()
-    setup_legend(axl,handles,labels)
+    setup_legend(axl,handles,labels )
 
     for n in range(nmodes):
         ss = axs[n].get_subplotspec()
+
+        # mplcyberpunk.make_lines_glow(axs[n])
+        # Title
         axs[n].set_title(f"Mode {n+1}")
+
+        # Xlabel
         if ss.is_last_row():
             axs[n].set_xlabel(
                 "Time (s)",
                 fontsize=14
             )
         else:
+            # Removing xtick labels for axes that are not at the bottom
             axs[n].set_xticklabels([])
+
+        # List of ax on the left of the plot
+        ax_left = []
+
+        # Ylabel
         if ss.is_first_col():
             axs[n].set_ylabel(
                 "Mode amplitude",
                 fontsize=14
             )
+            # Add ax to the list for aligning ylabel
+            ax_left.append(axs[n])
         # else:
         #     axs[n].set_yticks([])
 
+
+        # Ylim
+        # Use whatever formula that works to have nice
+        percent_eigen = 1.4
+        value_ylim = np.sqrt(eigen[n]) * percent_eigen
+        axs[n].set_ylim(-value_ylim,value_ylim)
+
+        # Removing unnecessary spines
         axs[n].spines["top"].set_visible(False)
         axs[n].spines["right"].set_visible(False)
 
+    fig.align_ylabels(ax_left)
+    fig.suptitle("Velocity Mode")
     plt.tight_layout()
     pr.save_plot(f"modes_{nmodes}.png")
 
 
 if __name__ == "__main__":
+
     pr = pyRedLUM(
         res_folder="/home/fregnault/data/red_lum_cpp_data/DNS100/ROMDNS-v3.4.1/ITHACAoutput/Reduced_coeff_4_0.001_100_neglectedPressure_centered",
-        save_dir="/home/fregnault/data/red_lum_cpp_data/DNS100/ROMDNS-v3.4.1/", verbose=1)
+        save_dir="/home/fregnault/data/red_lum_cpp_data/DNS100/ROMDNS-v3.4.1/",
+        verbose=0
+    )
 
 
     plot_all_modes(pr)
